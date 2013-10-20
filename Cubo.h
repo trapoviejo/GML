01caeaf6e6421fb2957c6fbb19bd1c9bad5f29b9
trapoviejo <pochart1990@gmail.com>
2013-10-20 12:31:02 -0500

    Agregue archivos cubo.h y cubo.cpp

---
+++ Cubo.h
@@ -0,0 +1,19 @@
+#ifndef GML_CUBO
+#define GML_CUBO
+
+
+class Cubo {
+
+public:
+
+
+    string verificaOperacion(int op1, int op2, int oper);        
+    
+
+    string cubo[16][7][7]; 
+  
+    
+    };
+    #endif
+    
+    
